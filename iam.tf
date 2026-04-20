3784f369a7a547583c6b6e2c6534aa8fc226c183
Rimoldeen <rimoldeenfrancis@gmail.com>
2026-04-20 16:28:29 +0100

    feat: updating role names

--- iam.tf
+++ iam.tf
@@ -31,7 +31,7 @@ data "aws_iam_policy_document" "replicator_assume" {
 }
 
 resource "aws_iam_role" "this" {
-  name               = "GDSCloudConsumption"
+  name               = "GDSCloudConsumption_v2_test"
   path               = "/service-role/"
   assume_role_policy = data.aws_iam_policy_document.replicator_assume.json
 }
@@ -80,7 +80,7 @@ data "aws_iam_policy_document" "replicator" {
 }
 
 resource "aws_iam_role_policy" "replicator" {
-  name   = "Replication"
+  name   = "Replication_v2_test"
   role   = aws_iam_role.this.id
   policy = data.aws_iam_policy_document.replicator.json
 }
